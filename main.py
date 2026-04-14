81408b46a00d85e985574d8fbb75eb0485165dc1
Goran Petrovic <smatic422@gmail.com>
2026-04-14 22:53:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 @app.post("/chat")
 async def chat(request: Request):
     data = await request.json()
