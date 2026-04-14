8f98a2480dd8bf00cced13b5e36325a4fa30b724
Goran Petrovic <smatic422@gmail.com>
2026-04-14 14:11:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Schweinchen ist online"}
+    return {"status": "online"}
 
 def start_server():
-    uvicorn.run("main:app", host="0.0.0.0", port=10000, reload=False)
+    uvicorn.run("main:app", host="0.0.0.0", port=10000)
 
 if __name__ == "__main__":
     start_server()
