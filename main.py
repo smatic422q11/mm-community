6ea21e28371801aadbba152aa6b8f6049677cf89
Goran Petrovic <smatic422@gmail.com>
2026-05-09 15:33:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 import requests 
 import os
+from pymongo import MongoClient
+
+MONGO_URI = os.environ.get('MONGO_URI')
+client = MongoClient(MONGO_URI)
+db = client['mm-community']
 
 app = FastAPI()
 
